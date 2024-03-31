b97a77b1ab9fdb4eac6f2aa7ff7ddb0086855663
kizumba <12.25andre@gmail.com>
2024-03-31 11:08:17 -0300

    Adicionado turma em models e feito migrate

--- escolaativa/main/models.py
+++ escolaativa/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Turma(models.Model):
+    id_turma = models.AutoField(primary_key=True)
+    turno_turma = models.CharField(max_length=20)
+    ano_turma = models.IntegerField()
+    serie_turma = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.id_turma
