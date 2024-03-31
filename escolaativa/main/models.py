7981c12b6bcafffd0859aed0fc7c5272138074a5
kizumba <12.25andre@gmail.com>
2024-03-31 19:38:43 -0300

    Testes com a classe Turma OK

--- escolaativa/main/models.py
+++ escolaativa/main/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Turma(models.Model):
     id_turma = models.AutoField(primary_key=True)
-    turno_turma = models.CharField(max_length=20)
-    ano_turma = models.IntegerField()
-    serie_turma = models.CharField(max_length=5)
+    serie = models.CharField(max_length=10)
+    periodo = models.CharField(max_length=15)
+    data_criacao = models.DateField()
 
     def __str__(self):
-        return self.id_turma
+        return f'Turma: Série: {self.serie}, período: {self.periodo} criada em {self.data_criacao}'
