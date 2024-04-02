d0c85051dac622c9f68460cbf62292c06308d5ea
kizumba <12.25andre@gmail.com>
2024-04-02 19:30:52 -0300

    Adicionado teste da pagina ajuda.html

--- escolaativa/main/tests.py
+++ escolaativa/main/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from django.urls import reverse
+
 # Create your tests here.
 class TestarPaginas(TestCase):
     def testar_se_pagina_principal_carrega_completamente(self):
@@ -7,3 +9,9 @@ class TestarPaginas(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'base.html')
         self.assertContains(response, 'Escola Ativa')
+
+    def testar_se_pagina_ajuda_carrega_completamente(self):
+        response = self.client.get(reverse("ajuda"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertContains(response, '<h2>Ajuda</h2>')
